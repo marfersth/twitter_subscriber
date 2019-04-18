467370d55deaff22b608946b8b24fd3a7428d70c
martin f <marfersth@gmail.com>
2019-04-18 14:42:12 -0300

    add json format to endpoint responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::API
   def log_twitter_account_activity_get
     logger.info '####### GET #######'
     logger.info params
-    head :ok
+    head :ok, format: :json
   end
 
   def log_twitter_account_activity_post
     logger.info '####### POST #######'
     logger.info params
-    head :ok
+    head :ok, format: :json
   end
 end

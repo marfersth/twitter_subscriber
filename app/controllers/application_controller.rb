8651ece922b6ff07aebd663064f43a57ae0a11d2
martin f <marfersth@gmail.com>
2019-04-18 14:28:45 -0300

    add get route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::API
-  def log_twitter_account_activity
+  def log_twitter_account_activity_get
+    logger.info '####### GET #######'
+    logger.info params
+    head :ok
+  end
+
+  def log_twitter_account_activity_post
+    logger.info '####### POST #######'
     logger.info params
     head :ok
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/log_twitter_account_activity' => 'application#log_twitter_account_activity'
+  post '/log_twitter_account_activity' => 'application#log_twitter_account_activity_post'
+  get '/log_twitter_account_activity' => 'application#log_twitter_account_activity_get'
 end

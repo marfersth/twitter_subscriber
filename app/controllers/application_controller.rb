5f8709794f510d4079fce03c2e994fb362d73ce8
martin f <marfersth@gmail.com>
2019-04-19 13:07:26 -0300

    add arc token verification in controller get method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,26 @@ class ApplicationController < ActionController::API
   def log_twitter_account_activity_get
     logger.info '####### GET #######'
     logger.info params
-    head :ok, format: :json
+
+    crc_token = params['crc_token']
+    if not crc_token.nil?
+      response = {}
+      consumer_secret = 'EduPumcCfrXVjC3pBpiPUXHQiuCJiRNtuvBuM2MF5MoDb2phKC'
+      response['response_token'] = "sha256=#{generate_crc_response(consumer_secret, crc_token)}"
+      body response.to_json
+    end
+
+    status 200
   end
 
   def log_twitter_account_activity_post
     logger.info '####### POST #######'
     logger.info params
     head :ok, format: :json
+  end
+
+  def generate_crc_response(consumer_secret, crc_token)
+    hash = OpenSSL::HMAC.digest('sha256', consumer_secret, crc_token)
+    return Base64.encode64(hash).strip!
   end
 end

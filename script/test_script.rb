5d24586a43362307aa90c3c9dd23c6414658626a
Martin Fernandez <marfersth@gmail.com>
2020-05-27 17:00:56 -0300

    Update test_script.rb

--- script/test_script.rb
+++ script/test_script.rb
@@ -13,7 +13,7 @@ tm.get_webhook_configs
 # curl --request POST
 # --url 'https://api.twitter.com/1.1/account_activity/all/:ENV_NAME/webhooks.json?url=https%3A%2F%2Fflimper-twitter-test.herokuapp.com%2Flog_twitter_account_activity'
 # --header 'authorization: OAuth
-# oauth_consumer_key="SAOdqFOUcnfGGiKs7uC4TVe3S",
+# oauth_consumer_key="SAGiKs7uC4TVe3S",
 # oauth_nonce="GENERATED",
 # oauth_signature="GENERATED",
 # oauth_signature_method="HMAC-SHA1",
@@ -22,4 +22,4 @@ tm.get_webhook_configs
 # oauth_version="1.0"'
 
 
-# Created webhook instance with webhook_id: 1119273318631256065 | pointing to https://flimper-twitter-test.herokuapp.com/log_twitter_account_activity
+# Created webhook instance with webhook_id: 111927331256065 | pointing to https://flimper-twitter-test.herokuapp.com/log_twitter_account_activity

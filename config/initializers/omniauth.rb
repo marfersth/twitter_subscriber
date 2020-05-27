aec213ddf7c5bf787ca55b2d50fec49d981f6168
Martin Fernandez <marfersth@gmail.com>
2020-05-27 16:56:10 -0300

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'eKI4sPLbzBTcPQQchRl0q0pUF', 'GR3PiJEUTFxK0wSxwddxf5FrTAjVChJehdB5t6GpQKnBYNd0vM'
+  provider :twitter, 'key1', 'key2'
 end
